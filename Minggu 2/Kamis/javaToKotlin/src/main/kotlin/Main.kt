6b14dd628ab69930efe0153a6f323d9aae7bb1d7
Rizki Darmawan <rizkidarmawan.0402102@gmail.com>
2022-03-11 15:07:00 +0700

    feat: adding task javaToKotlin OOP

---
+++ Minggu 2/Kamis/javaToKotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+class mobil {
+    var warna: String = ""
+    var tahun: Int = 0
+
+}
+fun main(){
+    val mobil1 = mobil()
+    mobil1.warna = "Hijau"
+    mobil1.tahun = 2022
+    println("Warna Mobil : ${mobil1.warna}")
+    println("Tahun Mobil : ${mobil1.tahun}")
+
+}
