be356f21fdf2830733969e93984dcf749e717291
Rizki Darmawan <rizkidarmawan.0402102@gmail.com>
2022-05-18 09:02:05 +0700

    add: task week 6

--- Minggu 3/buku/src/main/kotlin/Main.kt
+++ Minggu 3/buku/src/main/kotlin/Main.kt
@@ -1,31 +1,49 @@
-class buku(){
-    var judul: String = ""
-    var pengarang: String = ""
-    var penerbit: String = ""
-    var tahun: Int = 0
+//class buku(){
+//    var judul: String = ""
+//    var pengarang: String = ""
+//    var penerbit: String = ""
+//    var tahun: Int = 0
+//
+//    // create contructor for judul,pengarang, penerbit, tahun
+//    constructor(judul: String, pengarang: String, penerbit: String, tahun: Int): this(){
+//        this.judul = judul
+//        this.pengarang = pengarang
+//        this.penerbit = penerbit
+//        this.tahun = tahun
+//    }
+//
+//    // create method for print
+//    fun cetakBuku(){
+//        println("Judul: $judul")
+//        println("Pengarang: $pengarang")
+//        println("Penerbit: $penerbit")
+//        println("Tahun: $tahun")
+//    }
+//}
+//
+//fun main(args: Array<String>) {
+//    // create object buku
+//    val buku1 = buku("Kotlin dasar", "Dwi", "Gramedia", 2019)
+//    buku1.cetakBuku()
+//    println(" ")
+//    val buku2 = buku("Programmer Zaman Now", "Hilman", "Gramedia", 2022)
+//    buku2.cetakBuku()
+//}
 
-    // create contructor for judul,pengarang, penerbit, tahun
-    constructor(judul: String, pengarang: String, penerbit: String, tahun: Int): this(){
-        this.judul = judul
-        this.pengarang = pengarang
-        this.penerbit = penerbit
-        this.tahun = tahun
-    }
 
-    // create method for print
-    fun cetakBuku(){
-        println("Judul: $judul")
-        println("Pengarang: $pengarang")
-        println("Penerbit: $penerbit")
-        println("Tahun: $tahun")
-    }
-}
+package bot
 
-fun main(args: Array<String>) {
-    // create object buku
-    val buku1 = buku("Kotlin dasar", "Dwi", "Gramedia", 2019)
-    buku1.cetakBuku()
-    println(" ")
-    val buku2 = buku("Programmer Zaman Now", "Hilman", "Gramedia", 2022)
-    buku2.cetakBuku()
+import java.util.Scanner
+
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    println("Hello! My name is Aid.")
+    println("I was created in 2020.")
+    println("Please, remind me your name.")
+
+    // reading a name
+
+    println("What a great name you have, ${scanner.next()}!")
 }
