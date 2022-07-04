84bde26c1f67707843caaa630490dea3532aee2d
Rizki Darmawan <rizkidarmawan.0402102@gmail.com>
2022-07-04 18:26:09 +0700

    feat: UAS

--- Minggu 10/Check/src/main/kotlin/Main.kt
+++ Minggu 10/Check/src/main/kotlin/Main.kt
@@ -1,48 +1,52 @@
-fun factorial(num: Int): Long {
-    var result = 1L
-    for (i in 2..num){
-        println("$result x "+ i)
-        result *= i
-    }
-
-    return result
-}
-
-fun polindrom(num: Int){
-    var num = num
-    var reversedInteger = 0
-    var remainder: Int
-    val originalInteger: Int
-
-    originalInteger = num
-
-    while (num != 0) {
-        remainder = num % 10
-        reversedInteger = reversedInteger * 10 + remainder
-        num /= 10
-    }
-
-    if (originalInteger == reversedInteger)
-        println("$originalInteger is true")
-    else
-        println("$originalInteger is false")
-}
-
-fun searchArray(array: Array<Int>, search: Int){
-    for (i in array) {
-        if (i % 3 === 0){
-            println("Element $i ditemukan di index ke "+ array.indexOf(i) +" merupakan kelipatan bilangan $search.")
-        }
-    }
-}
-
-
-fun main(args: Array<String>) {
-    println("===== Polindrome =====")
-    polindrom(1234321)
-    println("===== Factiorial =====")
-    println(factorial(4))
-    println("===== Search Array =====")
-    println(searchArray(arrayOf(10,33,24,21,20),3))
-
-}
+//fun factorial(num: Int): Long {
+//    var result = 1L
+//    for (i in 2..num){
+//        println("$result x "+ i)
+//        result = result * i
+//    }
+////    1 x 2
+////    2 x 3
+////    6 x 4
+////    24 X 5
+////    120
+//    return result
+//}
+//
+//fun polindrom(num: Int){
+//    var num = num
+//    var reversedInteger = 0
+//    var remainder: Int
+//    val originalInteger: Int
+//
+//    originalInteger = num
+//
+//    while (num != 0) {
+//        remainder = num % 10
+//        reversedInteger = reversedInteger * 10 + remainder
+//        num /= 10
+//    }
+//
+//    if (originalInteger == reversedInteger)
+//        println("$originalInteger is true")
+//    else
+//        println("$originalInteger is false")
+//}
+//
+//fun searchArray(array: Array<Int>, search: Int){
+//    for (i in array) {
+//        if (i % 3 === 0){
+//            println("Element $i ditemukan di index ke "+ array.indexOf(i) +" merupakan kelipatan bilangan $search.")
+//        }
+//    }
+//}
+//
+//
+//fun main(args: Array<String>) {
+//    println("===== Polindrome =====")
+//    polindrom(1234321)
+//    println("===== Factiorial =====")
+//    println(factorial(4))
+//    println("===== Search Array =====")
+//    println(searchArray(arrayOf(10,33,24,21,20),3))
+//
+//}
